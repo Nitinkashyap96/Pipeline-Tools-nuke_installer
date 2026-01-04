b9e2e1f25a855f32d51c57f54f7283d48c1f0e34
Nitin Kashyap <kashyapaniket542@gmail.com>
2026-01-04 18:04:26 +0530

    Update menu.py

--- PipelineCore_nuke_installer/menu.py
+++ PipelineCore_nuke_installer/menu.py
@@ -24,7 +24,7 @@ nuke.pluginAddPath(r'./icon')
 # Place this in your ~/.nuke/menu.py file
 
 # 1. Import your installer module. Adjust import path if necessary.
-import PipelineCore_nuke_installer  # Use the filename of your finalized installer
+import PipelineCore_nuke_installer_2  # Use the filename of your finalized installer
 
 # 2. Add a menu item in a custom menu:
 toolbar = nuke.menu("Nuke")  # Top-level
@@ -33,7 +33,7 @@ github_menu = toolbar.addMenu("GitHub Tools", icon="GitHub.png.png")
 # 3. Add a command that opens the installer dialog
 github_menu.addCommand(
     "Install GitHub Repo...",  # Menu command name
-    "PipelineCore_nuke_installer.show_installer()",  # Function to call
+    "PipelineCore_nuke_installer_2.show_installer()",  # Function to call
     icon="GitHub_Logo_W.png"
 )
 
@@ -48,4 +48,5 @@ github_menu.addCommand(
 
 
 license ="Copyright (C) 2025 by Niti Kashyap,All rights reserved."
+
 nuke.tprint(f"PipelineCore_nuke_installer {version},  build  {update_date}. \n{license}")
